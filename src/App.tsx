1f2acaca2a9251c8a20bff179cfde31eca058a57
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-08-05 01:40:36 +0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import {createContext, Dispatch, FormEvent, SetStateAction, useContext, useState} from 'react'
+import './App.css'
+
+type Auth = {
+    token: string | null,
+    profile: string | null,
+    setToken?: Dispatch<SetStateAction<string|null>>,
+    setProfile?: Dispatch<SetStateAction<string|null>>
+}
+
+const AuthContext = createContext({token: null, profile: null} as Auth);
+
+function App() {
+    const auth = getAuthFromStorage();
+    const [token, setToken] = useState(auth.token);
+    const [profile, setProfile] = useState(auth.profile);
+    return (
+        <AuthContext.Provider value={{token, setToken, profile, setProfile}}>
+            <Header></Header>
+            <Content></Content>
+        </AuthContext.Provider>
+    )
+}
+
+function getAuthFromStorage(): Auth {
+    return {token: null, profile: null} as Auth;
+}
+
+export default App
+
+type HeaderProps = {
+    onLogin: (username: string, password: string) => Promise<void>
+}
+
+function Header(props) {
+    const auth = useContext<Auth>(AuthContext);
+
+    return (
+        <div className="header">
+            <div className="logo">
+                <h1>Neto Social</h1>
+            </div>
+            {!auth.token ? (<HeaderLoginForm></HeaderLoginForm>) : (<HeaderProfileWidget></HeaderProfileWidget>)}
+        </div>
+    )
+}
+
+type HeaderLoginFormProps = {
+    onLogin: (username: string, password: string) => Promise<void>
+}
+
+function HeaderLoginForm(props: HeaderLoginFormProps) {
+    const {onLogin} = props;
+    const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const username = (event.currentTarget[0] as HTMLInputElement).value;
+        const password = (event.currentTarget[1] as HTMLInputElement).value;
+        onLogin(username, password)
+            .catch(e => {
+                alert(e.message);
+            })
+    }
+    return (
+        <form className="login-form" onSubmit={onSubmit}>
+            <input type="text" placeholder="username"></input>
+            <input type="text" placeholder="password"></input>
+            <input type="submit" value="Login"></input>
+        </form>
+    )
+}
+
+function HeaderProfileWidget(props) {
+    return (
+        <div>Профиль юзера</div>
+    )
+}
+
+function Content(props) {
+    return (
+        <div>Content</div>
+    )
+}
